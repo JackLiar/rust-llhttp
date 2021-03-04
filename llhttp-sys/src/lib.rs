02878f2df99993a08bb5f7b6f44366fd7363ac48
Jack Liar <>
2021-03-04 22:02:44 +0800

    supress compiler warnings

--- llhttp-sys/src/lib.rs
+++ llhttp-sys/src/lib.rs
@@ -1,4 +1,7 @@
 #![no_std]
-#![allow(non_camel_case_types, clippy::unreadable_literal)]
+#![allow(clippy::unreadable_literal)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/llhttp.rs"));
