606fce1a0c8517fd931291964cfec57fbcf55c0c
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-06-07 20:15:16 +0800

    wrapper macros take safer params

--- llhttp/src/lib.rs
+++ llhttp/src/lib.rs
@@ -24,8 +24,13 @@ unsafe impl Send for Settings {}
 #[macro_export]
 macro_rules! cb_wrapper {
     ($fname:ident, $func:ident) => {
+        #[inline]
         unsafe extern "C" fn $fname(arg1: *mut llhttp::ffi::llhttp_t) -> libc::c_int {
-            $func(&mut *(arg1 as *mut llhttp::Parser))
+            let parser = &mut *(arg1 as *mut llhttp::Parser);
+            match $func(parser) {
+                Ok(_) => 0,
+                Err(_) => -1,
+            }
         }
     };
 }
@@ -33,12 +38,18 @@ macro_rules! cb_wrapper {
 #[macro_export]
 macro_rules! data_cb_wrapper {
     ($fname:ident, $func:ident) => {
+        #[inline]
         unsafe extern "C" fn $fname(
             arg1: *mut llhttp::ffi::llhttp_t,
             at: *const ::libc::c_char,
             length: usize,
         ) -> libc::c_int {
-            $func(&mut *(arg1 as *mut llhttp::Parser), at, length)
+            let parser = &mut *(arg1 as *mut llhttp::Parser);
+            let data = std::slice::from_raw_parts(at, length);
+            match $func(parser, data) {
+                Ok(_) => 0,
+                Err(_) => -1,
+            }
         }
     };
 }
